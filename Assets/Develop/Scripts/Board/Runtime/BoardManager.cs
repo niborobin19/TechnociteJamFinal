b3162bb06c03874d912d90c465f6b933aac0f25d
niborobin19 <56249261+niborobin19@users.noreply.github.com>
2021-06-29 13:38:07 +0200

    [Develop/Suspect] board setup

---
+++ Assets/Develop/Scripts/Board/Runtime/BoardManager.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+namespace 
+{
+    public class BoardManager : MonoBehaviour
+    {
+        #region Exposed
+
+        #endregion
+
+
+        #region Unity API
+
+        #endregion
+
+
+        #region Private
+
+        #endregion
+    }
+}
